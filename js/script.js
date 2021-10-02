6e625144af61204c49f3fe8bf138715c87afd829
Nancy Chaudhry <nancych14@gmail.com>
2021-10-02 08:30:40 +0500

    Validating letter input

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ const message = document.querySelector(".message");
 const playAgainBtn = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const wordsInProgressText = function(word) {
     let letterArray = [];
     for(let i = 0; i < word.length; i++){ 
-        console.log(word[i]);
         letterArray.push("●")
     }
     wordInProgress.innerHTML = letterArray.join('');
@@ -20,9 +20,39 @@ const wordsInProgressText = function(word) {
 
 wordsInProgressText(word);
 
+const validateInput = function(letterGuess) {
+    const acceptedLetter = /[a-zA-Z]/;
+
+    if (letterGuess === "") {
+        message.innerHTML = "Please enter a letter from A to Z"
+    } else if (letterGuess.length > 1) {
+        message.innerHTML = "Please enter only one letter at a time."
+    } else if (!letterGuess.match(acceptedLetter)) {
+        message.innerHTML = "Please enter a letter only."
+    } else if (letterGuess.match(acceptedLetter)) {
+        message.innerHTML = "Success! That's a valid letter!"
+        let validLetter = letterGuess;
+        return validLetter;
+    }
+};
+
+const makeGuess = function(validLetterInput) {
+    let validLetterUpper = validLetterInput.toUpperCase();
+    if (guessedLetters.includes(validLetterUpper)) {
+        message.innerHTML = "You've guessed that letter already.  Try another."
+    } else {
+        guessedLetters.push(validLetterUpper);
+        console.log(guessedLetters);
+    }
+}
+
 guessBtn.addEventListener('click', function(e) {
     e.preventDefault();
     let letterGuess = letterInput.value;
+    let validLetterInput = validateInput(letterGuess);
+    if (validLetterInput) {
+        console.log(validLetterInput);
+        makeGuess(validLetterInput);
+    }
     letterInput.value = "";
-    console.log(letterGuess);
 })
